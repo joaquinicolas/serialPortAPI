741a5331b29473b5c5b8e52f05045cea06312c74
Joaquin Noe <joako.n96@gmail.com>
2017-04-17 14:30:56 -0300

    last

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	
-	"io/ioutil"
-	"os"
+
+	"github.com/joaquinicolas/Elca/router"
 )
 
 func main() {
-	
-	router := CreateRouter()
-	log.Fatal(http.ListenAndServe(":8080", router))
 
+	r := router.CreateRouter()
+	fmt.Println("Listening on port 8080. http://127.0.0.1:8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
-
